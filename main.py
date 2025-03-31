19be2694dce38adbd582c633a1ca877da73a0d0c
prodesk98 <profissional.mateus.andrade@gmail.com>
2025-03-31 06:53:33 -0300

    Refine summarization prompt and streamline chat message handling

--- main.py
+++ main.py
@@ -14,21 +14,12 @@ st.write(
     "Você pode colar um texto longo e o modelo irá gerar um resumo ou extrair informações específicas."
 )
 
-if "messages" not in st.session_state:
-    st.session_state.messages = []
-
-for message in st.session_state.messages:
-    with st.chat_message(message["role"]):
-        st.markdown(message["content"])
-
 if prompt := st.chat_input(""):
-    st.session_state.messages.append({"role": "user", "content": prompt})
     with st.chat_message("user"):
         st.markdown(prompt)
 
     with st.chat_message("assistant"):
         stream = client.generate(prompt)
         response = st.write_stream(stream)
-    st.session_state.messages.append({"role": "assistant", "content": response})
 
 
